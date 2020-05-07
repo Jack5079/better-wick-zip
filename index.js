d3f31eaa198a2ced66251bbafc021611621b24f9
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-06 22:39:42 -0400

    migrate a ton

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 /* eslint-env browser */
-
-const worker = new Worker('./worker.js')
-
-worker.addEventListener('message', ({data: {blob, name}}) => {
-  const link = document.createElement('a')
-  link.href = URL.createObjectURL(blob)
-  link.download = name + '.zip'
-  link.style.display = 'none'
-  document.body.appendChild(link)
-  link.click()
-  URL.revokeObjectURL(link.href)
-})
+/* global JSZip */
+const indexTemplate = fetch('./template/index.html').then(res=>res.text()).then(txt=>new DOMParser().parseFromString(txt, 'text/html'))
+const gameTemplate = fetch('./template/game.html').then(res=>res.text())
+const pageProm = Promise.all([
+  'game.css',
+  'noscript.html',
+  'style.css'
+].map(async url=>[url, await fetch(`./template/${url}`).then(res=>res.blob())]))
+async function addProject (proj) {
+  console.log('Unzipping...')
+  const project = await new JSZip().loadAsync(await proj.arrayBuffer())
+  console.log('Importing...')
+  const indexDocument = await indexTemplate
+  const {project: {width, height, name}} = JSON.parse(await project.files['project.json'].async('string'))
+  indexDocument.getElementById('game').width = width
+  indexDocument.getElementById('game').height = height
+  indexDocument.querySelector('title').innerText = name
+  console.log('Zip created.')
+  const zip = new JSZip()
+  zip.file('index.html', indexDocument.documentElement.outerHTML)
+  zip.file('game.html', ( await gameTemplate).replace('{{TITLE}}', name))
+  zip.file('project.wick', proj)
+  zip.file('wickengine.js', fetch('https://raw.githubusercontent.com/Wicklets/wick-editor/master/engine/dist/wickengine.js').then(res=>res.blob()))
+  const pages = await pageProm
+  pages.forEach( arr => zip.file(...arr))
+  return {
+    name,
+    blob: await zip.generateAsync({type: 'blob'})
+  }
+}
 
 document.getElementById('upload').addEventListener('change', event => {
-  for (const file of event.target.files) {
-    worker.postMessage(file)
-    console.log(file)
-  }
+  const files = [...event.target.files]
+  Promise.all(files.map(addProject)).then(arr=>{
+    arr.forEach(({name, blob})=>{
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = name + '.zip'
+      link.style.display = 'none'
+      document.body.appendChild(link)
+      link.click()
+      URL.revokeObjectURL(link.href)
+    })
+  })
 })
